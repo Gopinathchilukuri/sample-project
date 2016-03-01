5bdef5166609bf4ffbd7e1c58c680c848a78e168
Gopinathchilukuri <gopinath.chilukuri@winbold.com>
2016-03-01 11:33:23 +0530

    commit 5

--- src/com/sample/controllers/StartApplication.java
+++ src/com/sample/controllers/StartApplication.java
@@ -1,7 +1,10 @@
 package com.sample.controllers;
 
 public class StartApplication {
-
+	/**
+	 * Its starting point.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to new programme.");
