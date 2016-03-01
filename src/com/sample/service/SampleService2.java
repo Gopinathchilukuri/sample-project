794bc190a46278800e6df82d4edb7ad103cd2356
Gopinathchilukuri <gopinath.chilukuri@winbold.com>
2016-03-01 11:19:45 +0530

    4 new message

---
+++ src/com/sample/service/SampleService2.java
@@ -0,0 +1,8 @@
+package com.sample.service;
+
+public class SampleService2 {
+	public String getSomeMessage(){
+		System.out.println("we are in getSomeMessge Service.");
+		return "we are in getSomeMessge Service.";
+	}
+}
